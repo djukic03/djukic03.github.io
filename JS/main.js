83afb7c8b80eb55316fa4f6499f99965ecdd3f68
djukic03 <djukic.nemanja003@gmail.com>
2024-09-15 17:04:15 +0200

    Home page finished

--- JS/main.js
+++ JS/main.js
@@ -1,30 +1,73 @@
 import { translations } from "./translation.js";
-var LanguageSelect = document.querySelector(".language");
-LanguageSelect.addEventListener("change", function () {
-  const selectedLang = this.value;
-  const translation = translations[selectedLang];
+document.querySelectorAll(".language").forEach((element) => {
+  element.addEventListener("change", function () {
+    const selectedLang = this.value;
+    sessionStorage.setItem("selectedLanguage", selectedLang);
+    const translation = translations[selectedLang];
 
-  document.querySelectorAll(".home").forEach((element) => {
-    element.innerHTML = translation.home;
+    document.querySelectorAll(".home").forEach((element) => {
+      element.innerHTML = translation.home;
+    });
+    document.querySelectorAll(".about").forEach((element) => {
+      element.innerHTML = translation.about;
+    });
+    document.querySelectorAll(".my-projects").forEach((element) => {
+      element.innerHTML = translation.projects;
+    });
+    document.querySelectorAll(".contact").forEach((element) => {
+      element.innerHTML = translation.contact;
+    });
+    document.querySelector("#text-h2").innerHTML = translation.text_h2;
+    document.querySelector("#text-p").innerHTML = translation.text_p;
+    document.querySelector("#btn-contact").innerHTML = translation.btn_contact;
+    document.querySelector("#currently-working").innerHTML =
+      translation.currently_working;
+    document.querySelector("#about-me-p1").innerHTML = translation.about_me_p1;
+    document.querySelector("#about-me-p2").innerHTML = translation.about_me_p2;
+    document.querySelector("#about-me-p3").innerHTML = translation.about_me_p3;
+    document.querySelector("#read-cv").innerHTML = translation.read_cv;
+    document.querySelector(".view-all").innerHTML = translation.view_all;
+    document.querySelector("#ath-x-desc").innerHTML = translation.ath_x_desc;
+    document.querySelectorAll(".view-more").forEach((element) => {
+      element.innerHTML = translation.view_more;
+    });
+    document.querySelector("#djuka-title").innerHTML = translation.djuka_title;
+    document.querySelector("#djuka-desc").innerHTML = translation.djuka_desc;
+    document.querySelector("#portfolio-title").innerHTML =
+      translation.portfolio_title;
+    document.querySelector("#portfolio-desc").innerHTML =
+      translation.portfolio_desc;
+    document.querySelector(".my-skills").innerHTML = translation.my_skills;
+    document.querySelector("#soft-dev").innerHTML = translation.soft_dev;
+    document.querySelector("#web-dev").innerHTML = translation.web_dev;
+    document.querySelector("#databases").innerHTML = translation.databases;
+    document.querySelector("#tools").innerHTML = translation.tools;
+    document.querySelector("#management").innerHTML = translation.management;
+    document.querySelector("#pm").innerHTML = translation.pm;
+    document.querySelector("#pdm").innerHTML = translation.pdm;
+    document.querySelector("#pe").innerHTML = translation.pe;
+    document.querySelector("#e").innerHTML = translation.e;
+    document.querySelector("#f").innerHTML = translation.f;
+    document.querySelector("#contact-text").innerHTML =
+      translation.contact_text;
+    document.querySelector("#name").placeholder = translation.name;
+    document.querySelector("#email").placeholder = translation.email;
+    document.querySelector("#subject").placeholder = translation.subject;
+    document.querySelector("#message").placeholder = translation.message;
+    document.querySelector("#send").innerHTML = translation.send;
+    document.querySelector("#or-contact").innerHTML = translation.or_contact;
+    document.querySelector("#footer-text").innerHTML = translation.footer_text;
   });
-  document.querySelectorAll(".about").forEach((element) => {
-    element.innerHTML = translation.about;
+});
+
+window.addEventListener("load", () => {
+  document.querySelectorAll(".language").forEach((element) => {
+    const selectedLang = sessionStorage.getItem("selectedLanguage");
+    if (selectedLang) {
+      element.value = selectedLang;
+      element.dispatchEvent(new Event("change"));
+    }
   });
-  document.querySelectorAll(".projects").forEach((element) => {
-    element.innerHTML = translation.projects;
-  });
-  document.querySelectorAll(".contact").forEach((element) => {
-    element.innerHTML = translation.contact;
-  });
-  document.querySelector("#text-h2").innerHTML = translation.text_h2;
-  document.querySelector("#text-p").innerHTML = translation.text_p;
-  document.querySelector("#btn-contact").innerHTML = translation.btn_contact;
-  document.querySelector("#currently-working").innerHTML =
-    translation.currently_working;
-  document.querySelector("#about-me-p1").innerHTML = translation.about_me_p1;
-  document.querySelector("#about-me-p2").innerHTML = translation.about_me_p2;
-  document.querySelector("#about-me-p3").innerHTML = translation.about_me_p3;
-  document.querySelector("#read-cv").innerHTML = translation.read_cv;
 });
 
 var MobileNav = document.querySelector(".mobile-nav");
@@ -57,3 +100,24 @@ MobileNav.addEventListener("click", function () {
     lineTwo.style.transform = "rotate(45deg)";
   }
 });
+
+(function () {
+  emailjs.init({
+    publicKey: "e3-_HYVJTgu2CHAjU",
+  });
+})();
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    emailjs.sendForm("contact_service", "contact_form", this).then(
+      () => {
+        alert("Email sent successfully");
+        document.getElementById("contactForm").reset();
+      },
+      (error) => {
+        alert("Error sending email. Please try again later. \n", error);
+      }
+    );
+  });
