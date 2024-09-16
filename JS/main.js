0560faeb4e14c697a51c55cd79a3953e7173422d
djukic03 <djukic.nemanja003@gmail.com>
2024-09-16 14:47:53 +0200

    About page work

--- JS/main.js
+++ JS/main.js
@@ -100,24 +100,3 @@ MobileNav.addEventListener("click", function () {
     lineTwo.style.transform = "rotate(45deg)";
   }
 });
-
-(function () {
-  emailjs.init({
-    publicKey: "e3-_HYVJTgu2CHAjU",
-  });
-})();
-
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    emailjs.sendForm("contact_service", "contact_form", this).then(
-      () => {
-        alert("Email sent successfully");
-        document.getElementById("contactForm").reset();
-      },
-      (error) => {
-        alert("Error sending email. Please try again later. \n", error);
-      }
-    );
-  });
