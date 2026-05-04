a7ca9c08a844500b23dcacee6cef4a75ecb5f9da
djukic03 <djukic.nemanja003@gmail.com>
2026-05-04 23:11:00 +0200

    Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "../CSS/main.css";
 import "../CSS/index.css";
 import "../CSS/about.css";
@@ -11,10 +11,10 @@ import { LanguageProvider } from "./context/LanguageContext";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
